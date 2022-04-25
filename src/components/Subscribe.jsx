309522d25211ba245705ae8b088027da9ebb9b31
unknown <mohdsaadben10@gmail.com>
2022-04-25 11:04:28 +0530

    final commit

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+import emailjs from "@emailjs/browser";
 
 export default function Subscribe() {
+  const [result, showResult] = useState(false);
+
+  // const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_g41qcwl",
+        "template_4b5rjlb",
+        e.target,
+        "eHpvDndZspF-z_HeX"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+    showResult(true);
+  };
+
   return (
     <div className="subscribe-form">
       <div className="container">
@@ -15,7 +43,7 @@ export default function Subscribe() {
             <div className="main-content">
               <p>Be the first to hear about new product launches!</p>
               <div className="container">
-                <form id="subscribe" action="" method="get">
+                <form id="subscribe" action="" onSubmit={sendEmail}>
                   <div className="row">
                     <div className="col-md-7">
                       <fieldset>
@@ -24,9 +52,6 @@ export default function Subscribe() {
                           type="text"
                           className="form-control"
                           id="email"
-                          onfocus="if(this.value == 'Your Email...') { this.value = ''; }"
-                          onBlur="if(this.value == '') { this.value = 'Your Email...';}"
-                          value="Your Email..."
                           required=""
                         />
                       </fieldset>
@@ -38,7 +63,9 @@ export default function Subscribe() {
                           id="form-submit"
                           className="button"
                         >
-                          Subscribe Now!
+                          {result
+                            ? "Thanks for subscribing PIXIE"
+                            : "Subscribe Now!"}
                         </button>
                       </fieldset>
                     </div>
